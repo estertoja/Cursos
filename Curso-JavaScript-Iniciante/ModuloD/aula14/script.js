b424f244b888c99aa1f54f864d6c5c5ece11192c
Ester CardosoToja <63560521+estertoja@users.noreply.github.com>
2021-04-08 19:05:10 -0300

    ex. Hora do dia

--- Curso-JavaScript-Iniciante/ModuloD/aula14/script.js
+++ Curso-JavaScript-Iniciante/ModuloD/aula14/script.js
@@ -1,1 +1,30 @@
-window.alert("Olá");
+function carregar() {
+
+    var msg = window.document.getElementById('msg')
+    var imagem = window.document.getElementById('imagem')
+    var text = window.document.getElementById('text')
+
+    // Hora atual
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    var seg = data.getSeconds()
+    msg.innerHTML=`Agora são ${hora}:${min}:${seg} horas.` 
+
+    if (hora >= 0 && hora <= 12) {
+      //Bom dia
+      imagem.src ='manha.png' 
+      text.innerHTML=`Bom dia!!`
+      document.body.style.background= 'rgb(241, 202, 93)'
+    }else if (hora >= 13 && hora <= 18){
+      //Boa tarde
+      imagem.src ='tarde.png'
+      text.innerHTML=`Boa tarde!!`
+      document.body.style.background='rgb(255, 169, 9)'
+    } else {
+      //Boa noite
+      imagem.src ='noite.png'
+      text.innerHTML=`Boa noite!!`
+      document.body.style.background='rgb(56, 54, 172)'
+    }
+}
